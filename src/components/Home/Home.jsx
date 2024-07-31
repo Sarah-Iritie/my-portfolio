35961e23c9e3d603d68ae7363b81568440f98518
Sarah-Iritie <143420861+Sarah-Iritie@users.noreply.github.com>
2024-07-31 22:17:15 +0200

    arrow icon test

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -61,6 +61,99 @@ export default function Home() {
             )}
           </a>
         </div>
+        <div className="secondary">
+          <div className="secondary-icon">
+            <a href="#About">
+              {isDark ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2.3em"
+                  height="2.3em"
+                  viewBox="0 0 24 24"
+                >
+                  <g transform="rotate(-90 12 12)">
+                    <g
+                      fill="none"
+                      stroke="#fff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-dasharray="14"
+                        stroke-dashoffset="14"
+                        d="M19 12H5.5"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          dur="0.3s"
+                          values="14;0"
+                        />
+                      </path>
+                      <path
+                        stroke-dasharray="8"
+                        stroke-dashoffset="8"
+                        d="M5 12L10 17M5 12L10 7"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          begin="0.3s"
+                          dur="0.2s"
+                          values="8;0"
+                        />
+                      </path>
+                    </g>
+                  </g>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2.3em"
+                  height="2.3em"
+                  viewBox="0 0 24 24"
+                >
+                  <g transform="rotate(-90 12 12)">
+                    <g
+                      fill="none"
+                      stroke="#1d1d1f"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-dasharray="14"
+                        stroke-dashoffset="14"
+                        d="M19 12H5.5"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          dur="0.3s"
+                          values="14;0"
+                        />
+                      </path>
+                      <path
+                        stroke-dasharray="8"
+                        stroke-dashoffset="8"
+                        d="M5 12L10 17M5 12L10 7"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          begin="0.3s"
+                          dur="0.2s"
+                          values="8;0"
+                        />
+                      </path>
+                    </g>
+                  </g>
+                </svg>
+              )}
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
